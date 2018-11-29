776ae549183c3f1be37e42e1f091d286ac18fa52
siriusformalhaut <44170802+siriusformalhaut@users.noreply.github.com>
2018-11-30 00:55:42 +0900

    ちょっと付け足した

--- codes/server/parkslab/manager/views.py
+++ codes/server/parkslab/manager/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
+
+from manager.models import Account
 
 # Create your views here.
+
+class AccountListView(TemplateView):
+    template_name = "worker_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(AccountListView, self)
+        return render(self.request, self.template_name, context)
--- codes/server/parkslab/parkslab/urls.py
+++ codes/server/parkslab/parkslab/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+import manager.views as manager_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^worker_list/', manager_view.AccountListView.as_view())
 ]
