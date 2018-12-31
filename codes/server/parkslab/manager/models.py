4f85dd16dffcc57f3b3b7193ccacd881f7d7fcbc
siriusformalhaut <44170802+siriusformalhaut@users.noreply.github.com>
2018-12-31 23:32:05 +0900

    DBに必要と考えられるカラム追加した

--- codes/server/parkslab/manager/models.py
+++ codes/server/parkslab/manager/models.py
@@ -11,6 +11,8 @@ class Account(models.Model):
     RESERCHER = 10
     GENERAL = 15
 
+    #ID
+    id = models.BigIntegerField()
     #名前
     name = models.CharField(max_length = 128)
     #アカウント名(本名明かしたくない人向け(一般のアカウントには需要ありそうなので追加)TODO 任意項目にする)
@@ -27,6 +29,36 @@ class Account(models.Model):
     institution = models.CharField(max_length = 128)
 
     #TODO 投げ銭した先の機関名を持たせたい。別テーブルでアカウント情報を紐づけて持たせる。
+    
+
+
+#投げ銭情報。TODO 別機能なのでそもそもちがう機能として持たせたほうがいいかも
+#TODO 投げ銭時系列情報も持たせる
+class GivingMoneyInfo(models.Model):
+
+    #アカウントID
+    #TODO 外部キー参照でアカウント情報と紐づける
+    accountid = models.BigIntegerField()
+    #所属機関
+    institution = models.CharField(max_length = 128)
+    #投げ銭した量の総計
+    givemoney = models.BigIntegerField()
+    #投げ銭された量の総計
+    getmoney = models.BigIntegerField()
+    
+#投げ銭時系列情報
+# TODO 投げ銭をした/された時間帯の管理  この金額の合計金額をGivingMonneyinfoに持たせる。この持たせ方でいいのか知らんけど。
+class GivingMoneyTimeSeriesInfo(models.Model):
+
+    #アカウントID
+    #TODO 外部キー参照でアカウント情報と紐づける
+    accoutid = models.BigIntegerField()
+    #所属機関
+    institution = models.CharField(max_length = 128)
+    #ある時間帯に投げ銭をした額の総量
+    givmoney = models.BigAutoField()
+    #投げ銭をした/された時間帯の情報
+    GivingMoneyTime = models.datetime()
 
 
 #ログイン機能実装の際に追記。今後要編集。
