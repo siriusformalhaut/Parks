f5ab66c19aec2dc2a2ecb42dd9de197e66b5d9c9
R-Yoshikawa <budgerigars163@gmail.com>
2019-04-09 21:31:57 +0900

    Anime test

--- codes/server/parkslab/manager/static/js/project_explore.js
+++ codes/server/parkslab/manager/static/js/project_explore.js
@@ -1,10 +1,33 @@
-var object = document.getElementById('astronomy');
-object.addEventListener('click',function()
-{
+/*
+// get objects whose classes are 'ltheme'
+lobjects = document.getElementsByClassName('ltheme');
+// for each object (note that 'forEach' is not applicable!)
+for (let i = 0; i < lobjects.length; ++i){
+    // onclick animation
+    lobjects[i].addEventListener('click',function()
+    {
+        anime({
+            targets: '#'+lobjects[i].id,
+            translateY: 250,
+            duration: 3000,
+            easing: 'linear'
+        });
+    });    
+}
+
+objects = document.getElementsByClassName('theme');
+objlen = objects.length;
+for (let i = 0; i < objlen; ++i){
+
     anime({
-        targets: '#astronomy',
-        translateX: 250,
-        duration: 3000,
-        easing: 'linear'
-    });
-});
+        targets: '#'+objects[i].id,
+        translateX: document.getElementById('explorefield').offsetWidth-objects[i].offsetWidth,
+        duration: 6000,
+        easing: 'linear',
+        autoplay: true,
+        loop: true,
+        delay: 1000*i/objlen 
+    })
+
+}
+*/
