e0ac135d8df6c1c6935aea38fdf29dd49839fa57
おせ　たつき <44314691+t-ose@users.noreply.github.com>
2019-04-14 18:57:02 +0900

    マージミス修正

--- codes/server/parkslab/manager/views.py
+++ codes/server/parkslab/manager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate
-from manager.models import UserAccount, Project, CategoryM
+from manager.models import UserAccount, Project, CategoryM, UserProfile
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
